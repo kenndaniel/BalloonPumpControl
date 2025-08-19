0ea04605c42047ddc8e869c1d9303bc23da74719
Ken Daniel <kenndaniel@yahoo.com>
2025-08-19 12:59:29 -0500

    Upgraded to latest version with diameter meter

--- BMP280_TempPres.h
+++ BMP280_TempPres.h
@@ -1,52 +1,78 @@
 
 
-#include <BMP280_DEV.h>                           // Include the BMP280_DEV.h library
+#include <BMP280_DEV.h> // Include the BMP280_DEV.h library
 
-float temperature, press, altitude;            // Create the temperature, pressure and altitude variables
-BMP280_DEV bmp280;                                // Instantiate (create) a BMP280_DEV object and set-up for I2C operation (address 0x77)
+float temperature, press, altitude; // Create the temperature, pressure and altitude variables
+BMP280_DEV bmp280;					// Instantiate (create) a BMP280_DEV object and set-up for I2C operation (address 0x77)
 
-float zeroPres=0.;  // Set to the first pressure reading in presSetup() then used as bias error
+float zeroPres = 0; // Reset to the first pressure reading in presSetup() then used as bias error
+int eeAddress = 0;	// EEPROM address to start reading from
 
 float pressure()
 {
-  bool good=false;
-  bmp280.startForcedConversion(); 
-  for (int i = 0; i < 250 ; ++i)
-  {
-                // Start BMP280 forced conversion (if we're in SLEEP_MODE)
+	bool good = false;
+	bmp280.startForcedConversion();
+	for (int i = 0; i < 250; ++i)
+	{
+		// Start BMP280 forced conversion (if we're in SLEEP_MODE)
 
-    if (bmp280.getMeasurements(temperature, press, altitude))    // Check if the measurement is complete
-    {
-      	// Serial.println();
-     	// Serial.print(temperature);                    // Display the results
-     	// Serial.print(F("*C   "));
-      	// Serial.print(press);
-      	// Serial.println(F("hPa   "));
-      	//Serial.print(altitude);
-      	//Serial.println(F("m"));
-		delay(100);
-	  return (press)*0.01450377-zeroPres;
-    }
-  }
+		if (bmp280.getMeasurements(temperature, press, altitude)) // Check if the measurement is complete
+		{
+			// Serial.println();
+			// Serial.print(temperature);                    // Display the results
+			// Serial.print(F("*C   "));
+			// Serial.print(press);
+			// Serial.println(F("hPa   "));
+			// Serial.print(altitude);
+			// Serial.println(F("m"));
+			delay(100);
+			return (press) * 0.01450377 - zeroPres;
+		}
+	}
 	Serial.println(" BMP 280 is humg *********************");
-	//bmp280.startForcedConversion(); 
+	//bmp280.startForcedConversion();
 	return 0.;
-
 }
 
-void presSetup()
+void presBegin()
 {
-  Serial.begin(9600);                           // Initialise the serial port
-  bmp280.begin(BMP280_I2C_ALT_ADDR);              // Default initialisation, place the BMP280 into SLEEP_MODE
-  //bmp280.setPresOversampling(OVERSAMPLING_X4);    // Set the pressure oversampling to X4
-  //bmp280.setTempOversampling(OVERSAMPLING_X1);    // Set the temperature oversampling to X1
-  //bmp280.setIIRFilter(IIR_FILTER_4); 
-  float press = pressure();             // Set the IIR filter to setting 4             
-  //Serial.print(" Init ");Serial.println(press);
-  zeroPres = press;
-  return  ;  // set the zero pressure on setup
-
+	bmp280.begin(BMP280_I2C_ALT_ADDR); // Default initialisation, place the BMP280 into SLEEP_MODE
+	// bmp280.setPresOversampling(OVERSAMPLING_X4);    // Set the pressure oversampling to X4
+	// bmp280.setTempOversampling(OVERSAMPLING_X1);    // Set the temperature oversampling to X1
+	// bmp280.setIIRFilter(IIR_FILTER_4);
+	// float press = pressure();             // Set the IIR filter to setting 4
+	// Serial.print(" Init ");Serial.println(press);
 }
+
+void presSetup(bool restart)
+{
+
+	if (restart)
+	{
+		EEPROM.get(eeAddress, zeroPres);
+	}
+	else
+	{
+		zeroPres = pressure();
+		EEPROM.put(eeAddress, zeroPres);
+		Serial.println( "Atmospheric pressure has been set.");
+	}
+
+	return; // set the zero pressure on setup
+}
+// void presSetup()
+// {
+//   Serial.begin(9600);                           // Initialise the serial port
+//   bmp280.begin(BMP280_I2C_ALT_ADDR);              // Default initialisation, place the BMP280 into SLEEP_MODE
+//   //bmp280.setPresOversampling(OVERSAMPLING_X4);    // Set the pressure oversampling to X4
+//   //bmp280.setTempOversampling(OVERSAMPLING_X1);    // Set the temperature oversampling to X1
+//   //bmp280.setIIRFilter(IIR_FILTER_4);
+//   float press = pressure();             // Set the IIR filter to setting 4
+//   //Serial.print(" Init ");Serial.println(press);
+//   //zeroPres = press;
+//   return  ;  // set the zero pressure on setup
+
+// }
 
 /* #include <Arduino.h>
 #include <Wire.h>
@@ -76,15 +102,15 @@ float pressure() {
 
 	for (int i=0; i<250; i++)
 	{ Serial.print("+ ");
-      delay(200);
+	  delay(200);
 	  if (bmx280.hasValue())
-      {
-      Serial.print("Pressure: "); Serial.println(bmx280.getPressure());
-      Serial.print("Pressure psi "); Serial.println((bmx280.getPressure64()-zeroPres)*0.0001450377);
-      //Serial.print("Temperature: "); Serial.println(bmx280.getTemperature());
-      return (bmx280.getPressure64()-zeroPres)*0.0001450377;
-      }
-    
+	  {
+	  Serial.print("Pressure: "); Serial.println(bmx280.getPressure());
+	  Serial.print("Pressure psi "); Serial.println((bmx280.getPressure64()-zeroPres)*0.0001450377);
+	  //Serial.print("Temperature: "); Serial.println(bmx280.getTemperature());
+	  return (bmx280.getPressure64()-zeroPres)*0.0001450377;
+	  }
+
    }
 
 	Serial.println(" BMP 280 is humg *********************");
@@ -93,9 +119,9 @@ float pressure() {
 }
 
 
-void presSetup() {
+void presBegin() {
   // put your setup code here, to run once:
-	Serial.begin(9600);
+	//Serial.begin(9600);
 
 	Wire.begin();
 
@@ -118,9 +144,10 @@ void presSetup() {
 
 	//by default sensing is disabled and must be enabled by setting a non-zero
 	//oversampling setting.
-	//set an oversampling setting for pressure and temperature measurements. 
+	//set an oversampling setting for pressure and temperature measurements.
 	bmx280.writeOversamplingPressure(BMx280MI::OSRS_P_x01);   //OSRS_P_x01  OSRS_P_x16
 	bmx280.writeOversamplingTemperature(BMx280MI::OSRS_T_x01);   //OSRS_T_x01  OSRS_T_x16
 	zeroPres = pressure()/0.0001450377;  // set the zero pressure on setup
 
-} */
+}
+ */
--- BalloonPump.ino
+++ BalloonPump.ino
@@ -15,172 +15,320 @@
  ********************************************************/
 #include <Arduino.h>
 #include <Wire.h>
-
-//#include <BMx280I2C.h>
+#include <EEPROM.h>
 #include <PID_v2.h>
 #include "BMP280_TempPres.h"
+// #include <string>
+using namespace std;
+// #include <SerialCommands.h>
+// #include "commands.h"
 
+// Gyro variables
+const int MPU_addr = 0x68;
+int16_t axis_X, axis_Y, axis_Z;
+
+int minVal = 265;
+int maxVal = 402;
+
+double x;
+double y;
+double z;
 
 #define PIN_INPUT 0
 #define RELAY_PIN 6
 
-
-//Define Variables we'll be connecting to
+// Define Variables we'll be connecting to
 float setpoint;
-#include "command_buffer.h"
-CCommandBuffer commandBuffer;
-#include "commandProcessing.h"
 
-//Specify the initial tuning parameters
-double Kp=20, Ki=.01, Kd=0;
-PID_v2 myPID( Kp, Ki, Kd, PID::Direct);
+// Specify the initial tuning parameters
+double Kp = 20, Ki = .0, Kd = 0;
+PID_v2 myPID(Kp, Ki, Kd, PID::Direct);
 int WindowSize = 250;
 unsigned long windowStartTime;
 
-float plow =0, phigh = .60, lowo = 0, higho = 1080;
+float readAngle()
+{ 
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 14, true);
+  axis_X = Wire.read() << 8 | Wire.read();
+  axis_Y = Wire.read() << 8 | Wire.read();
+  axis_Z = Wire.read() << 8 | Wire.read();
+  int xAng = map(axis_X, minVal, maxVal, -90, 90);
+  int yAng = map(axis_Y, minVal, maxVal, -90, 90);
+  int zAng = map(axis_Z, minVal, maxVal, -90, 90);
+
+  x = RAD_TO_DEG * (atan2(-yAng, -zAng) + PI);
+  y = RAD_TO_DEG * (atan2(-xAng, -zAng) + PI);
+  z = RAD_TO_DEG * (atan2(-yAng, -xAng) + PI);
+
+
+  return x;
+}
+
+float setPointAngle = 183.3;  // smaller numbers make a larger balloon
+void printAngle()
+{
+    Serial.print("Angle % ");
+    Serial.print(100.*readAngle()/setPointAngle);
+}
+
+
+
+float plow = 0, phigh = .40, lowo = 0, higho = 1080;
 
 float pmap(float p)
-{// Convert real world values (psi) to control variables
-float remap = p*(higho-lowo)/(phigh-plow);
-return remap;
+{ // Convert real world values (psi) to control variables
+  float remap = p * (higho - lowo) / (phigh - plow);
+  return remap;
 }
-float windowStartTime2 =0;
-#define ArraySize 22
+
+float windowStartTime2 = 0;
+#define ArraySize 18
 //
 //  TAKE THE CAP OFF THE BOTTLE WHEN STARTING UP TO CALIBRATE THE PRESSURE SENSOR TO ZERO
 //
-float Duration[ArraySize] = {180,180,60,60,60,45,45,45,45,45,45,45,45,45,45,45,90,90,90,90,90,60};  // ramp duration in minutes
-float setPoint[ArraySize] = {.05,.1,.2,.3,.35,.36,.37,.38,.39,.4,.41,.42,.43,.44,.45,.46,.465,.47,.45,.46,.43,0};   // pressures in psi
+float Duration[ArraySize] =  {60, 60, 40, 40, 40, 40, 40, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30, 30};                   
+ // ramp duration in minutes
+float setPoint[ArraySize] = {.05, .1, .18, .21, .24, .26, .28, .30, .31,.32, .33, .34, .35, .36, .37, .38, .39, .40}; 
+//pressures in psi
 float Times[ArraySize];
-int ip = 2;      // to pick up in the middle of the duration array, set ip to the starying index
+int ip = 0; // set ip to the starting index for restarts
 
-float startPoint = 0.;
+// Upper limit for any setpoint -
+float maxSetPoint = .40;
+
+float startPoint = .0;
 double setpt;
 
 bool firstTime = true;
 float setPointFunc()
-{// Ramp pressure up slowly
-  if (ip == ArraySize-1)  return pmap(setPoint[ArraySize-1]);
+{ // Ramp pressure up slowly
+  if (ip == ArraySize - 1)
+    return setPoint[ArraySize - 1];
 
   if (firstTime == true)
   {
-      float windowStartTime2 = (float)((millis()-windowStartTime2)/1000ul)/60.;
-      firstTime = false;
+    float windowStartTime2 = (float)((millis() - windowStartTime2) / 1000ul) / 60.;
+    firstTime = false;
   }
-  float timeMinute = (float)((millis()-windowStartTime2)/1000ul)/60.;
+  float timeMinute = (float)((millis() - windowStartTime2) / 1000ul) / 60.;
 
   float setptm = setPoint[ip];
-  float timeSwitch=0;
-  if (timeMinute > Times[ip] )
+  float timeSwitch = 0;
+  if (timeMinute > Times[ip])
   {
 
     timeSwitch = timeMinute;
     ip++;
   }
-  else if(ip > 0 && timeMinute < Times[ip-1])  // Happens when restarting with ip > 0
+  else if (ip > 0 && timeMinute < Times[ip - 1]) // Happens when restarting with ip > 0
   {
-     timeMinute = Times[ip-1];
+    timeMinute = Times[ip - 1];
   }
-  if( ip == ArraySize-1)
+  if (ip == ArraySize - 1)
   {
-    setptm = setPoint[ArraySize-1];
+    setptm = setPoint[ArraySize - 1];
   }
   else if (ip == 0)
   {
-     setptm = setPoint[0];
+    setptm = setPoint[0];
   }
   else
   {
-    setptm = setPoint[ip-1] + (setPoint[ip] - setPoint[ip-1]) * (1.-(Times[ip]-timeMinute)/(Times[ip]-Times[ip-1]));
+    setptm = setPoint[ip - 1] + (setPoint[ip] - setPoint[ip - 1]) * (1. - (Times[ip] - timeMinute) / (Times[ip] - Times[ip - 1]));
   }
-  
-    return setptm;
+
+  if (setptm > maxSetPoint)
+    setptm = maxSetPoint;
+
+  return setptm;
 }
 
+// ********************************* Setup *****************************************************
+// Set pres0 to the value of press when the valves and/or top are open
+float pres0 = 0.;
+// void readCommand()
 
+bool pressureError = false;
 void setup()
 {
   Serial.begin(9600);
-  //Serial.println(" Setup start ");
-  presSetup();
-  float press = pressure();  // Read the pressure
-  Serial.println(" Starting - Calibrating Open Valves ");
-  delay(15000);
-  
+  Wire.begin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+
+  presBegin();
+  Wire.beginTransmission(MPU_addr);
+  Wire.write(0x3B);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_addr, 14, true);
+  Serial.println(" Setup start ");
+
+  Serial.println("Set initial atmospheric pressure to be subtracted from pressure measurements");
+
+  Serial.println(F("\nType: 'new' to indicate starting a new balloon with zero pressure\n"
+                 "Type 'restart' to indicate restarting with a partial pressure balloon\n"
+                 "If you type the wrong value, remove power, release the pressure, start again and select new\n"
+                 " Then quickly close all valves."));
+
+  bool validInput = false;
+  while (true)
+  {
+    while (Serial.available() == 0)
+    {
+    }
+    String cmdInput = Serial.readString();
+    if (cmdInput.length() > 0)
+    {
+      if (cmdInput.startsWith(String('r')) )
+      { // Restarting a balloon with partial pressure
+        presSetup(true);
+        break;
+      }
+      else if (cmdInput.startsWith(String('n')) )
+      { // Stating from zero
+        presSetup(false);
+        break;
+      }
+    }
+
+    Serial.println("Please choose a valid selection either new or restart ");
+  }
+
+  Serial.print(" The maximum pressure is set to  ");
+  Serial.println(maxSetPoint);
+
+  float press = pressure(); // Read the pressure
+  Serial.print(" Current presure reading: press= ");
+  Serial.println(press);
+  int i;
+  for (i = 0; i < ArraySize; ++i)
+  { // set the initial set point
+    ip = i;
+    if (setPoint[i] > press)
+    {
+      ip = i - 1;
+      break;
+    }
+  }
+  if (i == ArraySize)
+  {
+    Serial.println("DANGER DANGER current pressure is too high - Something is wrong.");
+    pressureError = true;
+  }
   pinMode(RELAY_PIN, OUTPUT);
-   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   // set up times to switch
-  Times[0] = Duration[ip];  
-  for( int i=0;  i<ArraySize-1+ip; ++i)
-  {  
-    Times[i+1] = Times[i] + Duration[i+1+ip];
-
+  Times[0] = Duration[ip];
+  for (int i = 0; i < ArraySize - 1 + ip; ++i)
+  {
+    Times[i + 1] = Times[i] + Duration[i + 1 + ip];
   }
-  
+
   setpt = setPointFunc();
 
-
-  //tell the PID to range between 0 and the full window size
-  //Serial.println(" PID start ");
+  // tell the PID to range between 0 and the full window size
+  // Serial.println(" PID start ");
   myPID.SetOutputLimits(0, WindowSize);
-  myPID.Start(0,            // current input
-              0,            // current output
-              setpt);       // setpoint
+  myPID.Start(0,      // current input
+              0,      // current output
+              setpt); // setpoint
 
   windowStartTime = millis();
-
 }
 
+//**************************** LOOP ****************************
+float input = 0.;
 unsigned long previousMillis = 0;
 unsigned long interval = 2000;
+  float currentOut = 0.; 
+  float previousOut = 0.;
+  int iCnt=0;
+  
 void loop()
 {
- 
-  
-  float press = pressure();  // Read the pressure
 
-  float input = pmap(press);   // Convert the reading
+  if (pressureError == true)
+    return;
+
+  float angle = readAngle();
+ if (angle < setPointAngle)  // check the angle
+    {
+      maxSetPoint = setPoint[ip]; // decrease the pressure setpoint
+      iCnt--;
+      if (iCnt<0 ) iCnt=0;
+      digitalWrite(RELAY_PIN, LOW);
+      digitalWrite(LED_BUILTIN, LOW);
+      return;
+    }
+
+  float press = pressure(); // Read the pressure
+
+  input = pmap(press); // Convert the reading
   setpoint = setPointFunc();
   float stpt = pmap(setpoint);
-  float output = myPID.Run(input);     // call the controller function
-  myPID.Start(input,           // current input
-              output,          // current output
-              stpt); // setpoint
+  float output = myPID.Run(input); // call the controller function
+  myPID.Start(input,               // current input
+              output,              // current output
+              stpt);               // setpoint
   unsigned long currentMillis = millis();
-  if (currentMillis - previousMillis >= interval) 
+  currentOut = output/300.;
+  if (currentMillis - previousMillis >= interval)
   {
+   
+    printAngle();
     previousMillis = currentMillis;
-    Serial.print("Setpoint ");
-    Serial.print(setpoint );
+    Serial.print(" Setpoint ");
+    Serial.print(setpoint);
     Serial.print(",");
     Serial.print(" Pump ");
-    Serial.print(output/300. );
+    Serial.print(output / 300.);
     Serial.print(",");
     Serial.print(" Pressure ");
     Serial.print(press);
     Serial.print(",");
     Serial.print(" Time ");
-    Serial.print(currentMillis/(1000*3600));
+    Serial.print(currentMillis / (1000. * 3600.));
     Serial.print(",");
     Serial.print(" Index ");
     Serial.println(ip);
+    Serial.println(previousOut); 
+    if ((currentOut + previousOut) < .1) 
+    {
+      Serial.println(iCnt);
+      iCnt++;
+      if( iCnt > 30)
+      {
+        ip++;
+        iCnt=0;
+      }
+     }
+    previousOut = currentOut;
   }
   /************************************************
    * turn the output pin on/off based on pid output
    ************************************************/
-  while (millis() - windowStartTime > WindowSize) {
-    // time to shift the Relay Window
+  while (millis() - windowStartTime > WindowSize)
+  {
+    // time to shift the Relay
 
     windowStartTime += WindowSize;
   }
-  float diff = (float) (millis() - windowStartTime);
-  
+  float diff = (float)(millis() - windowStartTime);
 
   if (output > diff)
-    {digitalWrite(RELAY_PIN, HIGH);
-    digitalWrite(LED_BUILTIN, HIGH);}
+  {
+    digitalWrite(RELAY_PIN, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
+  }
   else
-    {digitalWrite(RELAY_PIN, LOW);
-    digitalWrite(LED_BUILTIN, LOW);}
+  {
+    digitalWrite(RELAY_PIN, LOW);
+    digitalWrite(LED_BUILTIN, LOW);
+  }
 }
